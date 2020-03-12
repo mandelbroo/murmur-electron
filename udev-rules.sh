96a09f886f5a5813195735096fe21e6d7f992534
vitalii <vitalii.makarets@scoutgaminggroup.com>
2020-03-12 12:17:20 +0200

    update udev-rules script

--- udev-rules.sh
+++ udev-rules.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-echo '"SUBSYSTEM=="usb", ATTRS{idProduct}=="f372", ATTRS{idVendor}=="04d8", MODE:="666"' > /etc/udev/rules.d/10-luxafor.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04d8", ATTR{idProduct}=="f372" MODE="0666"' > /etc/udev/rules.d/luxafor.rules
